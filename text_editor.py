83fd7b3f1000db1386ae731c54b2bd5d4d11968c
Sergio1260 <35770464+Sergio00166@users.noreply.github.com>
2023-11-05 17:41:36 +0100

    Update text_editor.py

--- text_editor.py
+++ text_editor.py
@@ -1,9 +1,9 @@
 #Code by Sergio1260
 
 from msvcrt import getch
-from os import get_terminal_size
+from os import get_terminal_size, getcwd
 from sys import argv
-from os import getcwd
+from os.path import exists
 
 def fixlenline(text, pointer):
     length=len(text)
@@ -14,8 +14,11 @@ if not len(argv)==1: filename=" ".join(argv[1:])
 else: filename=str(input("File to open: "))
 if not ":\\" in filename: filename=getcwd()+"\\"+filename
 
-tmp=open(filename, "r", encoding="UTF-8").readlines(); arr=[]
-for x in tmp: arr.append(x.replace("\r","").replace("\n","").replace("\f",""))
+if exists(filename):
+    tmp=open(filename, "r", encoding="UTF-8").readlines(); arr=[]
+    for x in tmp: arr.append(x.replace("\r","").replace("\n","").replace("\f",""))
+else: arr=[""]
+
 text=arr[0]; pointer=offset=0; line=banoff=2; arr.append("")
 black="[47m[30m[2m"; reset="[0m"; rows=get_terminal_size()[0]//5
 banner="\033c"+"█"*46+black+"BASIC TEXT EDITOR"+reset+"█"*46+"\n\n"
@@ -26,11 +29,15 @@ saved_txt=black+"SAVED"+reset; status=saved_df="█"*5; status_st=0
 while True:
     try:
         if pointer==0: pointer=1
+        if line==1: line=2
         extra=black+"File: "+filename+"  "+reset
         if status_st==0: status=saved_df
-        extra="█"*(len(extra)//2-9)+status+"█"*(len(extra)//2-9)+extra
+        extra_len=(96-len(extra))//2
+        extra="█"*extra_len+status+"█"*extra_len+extra
         arr[line+offset-banoff]=text; max_len=len(text)
-        print(banner+"\n".join(arr[offset:rows+offset+1])+bottom+extra+("\r\033[%d;%dH"%(line+1, pointer)),end="")
+        all_file="\n".join(arr[offset:rows+offset+1])
+        if len(all_file)<rows+1: all_file+="\n"*(rows-len(all_file))
+        print(banner+all_file+bottom+extra+("\r\033[%d;%dH"%(line+1, pointer)),end="")
         key=getch()
         if key==b'\xe0': #Directional arrows
             arrow_key=getch()
@@ -60,7 +67,7 @@ while True:
                 if not pointer==max_len+1:
                     p1=list(text); p1.pop(pointer-1)
                     text="".join(p1)
-                elif not line+offset==0: #move all to previous line
+                elif not line+offset==1: #move all to previous line
                     seltext=arr[line+offset-banoff+1]
                     arr[line+offset-banoff+1]=text+seltext
                     arr.pop(line+offset-banoff+1)
@@ -70,7 +77,7 @@ while True:
             if not pointer==1: #Delete char
                 p1=list(text); p1.pop(pointer-2)
                 text="".join(p1); pointer-=1
-            elif not line+offset==2: #move all to previous line
+            elif not line+offset==1: #move all to previous line
                 seltext=arr[line+offset-banoff-1]
                 arr[line+offset-banoff-1]=seltext+text
                 arr.pop(line+offset-banoff)
@@ -94,21 +101,19 @@ while True:
                     text=text[pointer-1:]
                     pointer=0    
                 else: arr.append("")
-            if not line+offset==len(arr): line+=1
+            if not line>rows+1: line+=1
             else: offset+=1
-            
+
         elif key==b'\x0f': #Ctrl + O (SAVE)
             out=open(filename,"w",encoding="UTF-8")
             out.write("\n".join(arr)); out.close()
             status=saved_txt; status_st=2
             
-        elif key==b'\x18': print("\033c",end=""); exit() #Ctrl + X (EXIT)
+        elif key==b'\x18': print("\033c",end=""); break #Ctrl + X (EXIT)
         
         else: #All the other keys
             p1=text[:pointer-1]; p2=text[pointer-1:]
             text=(p1+key.decode('utf-8')+p2)
             pointer+=1
         status_st-=1
-    except Exception as e: print("\033c"+str(e)); break
-
-input()
+    except: pass
