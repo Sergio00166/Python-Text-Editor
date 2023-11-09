8a673917d3d26768870d83f4261da9ce96845f7f
Sergio1260 <35770464+Sergio00166@users.noreply.github.com>
2023-11-09 17:54:33 +0100

    Mayor changes

--- pBTE.py
+++ pBTE.py
@@ -1,47 +1,9 @@
 #Code by Sergio1260
 
-from msvcrt import getch
-from os import get_terminal_size, getcwd
-from sys import argv
-from os.path import exists
-
-version="v0.1.2"
-
-#Dont ask me what it does
-def fixlenline(text, pointer, oldptr):
-    length=len(text)
-    if pointer>length or oldptr>length:
-        return length,oldptr
-    elif oldptr>pointer: return oldptr,oldptr
-    else: return pointer,oldptr
-
-#Check if we have arguments via cli, if not ask the user for a file to open
-if not len(argv)==1: filename=" ".join(argv[1:])
-else: filename=str(input("File to open: "))
-if not ":\\" in filename: filename=getcwd()+"\\"+filename
-
-#If file exist open it if not create an empty list
-if exists(filename): 
-    tmp=open(filename, "r", encoding="UTF-8").readlines(); arr=[]
-    for x in tmp: arr.append(x.replace("\r","").replace("\n","").replace("\f",""))
-    arr.append("")
-else: arr=[""]
-
-#Define a lot of stuff
-text=arr[0]; pointer=offset=0; line=banoff=2
-black="[47m[30m[2m"; reset="[0m"; rows=get_terminal_size()[0]//5
-banner="█"*8+black+"pBTE "+version+reset
-bottom="\n\n "+black+"^Q"+reset+" EXIT    "+black+"^S"+reset+" SAVE    "
-bottom+=black+"^A"+reset+" Save as    "+black+"^X"+reset+" CUT    "
-bottom+=black+"^C"+reset+" COPY    "+black+"^P"+reset+" PASTE    "
-bottom+=black+"^G"+reset+" GOTO    "
-copy_buffer=""; cls="\033c"; fix=False; oldptr=0
-
-#Flag to show after saving the file
-saved_txt=black+"SAVED"+reset; status=saved_df="█"*5; status_st=0
+from init import *
 
 while True:
-    #try:
+    try:
         #Fix some things every time
         if len(arr)==0: arr.append("")
         if pointer==0: pointer=1
@@ -60,17 +22,12 @@ while True:
         if key==b'\xe0': #Directional arrows
             special_key=getch()
             if special_key==b'H': #Up
-                if not line==banoff: line-=1
-                elif offset>0: offset-=1
-                text=arr[line+offset-banoff]
-                pointer,oldptr=fixlenline(text, pointer, oldptr)
+                pointer, oldptr, text, offset, line =\
+                up(line, offset, arr, text, banoff, oldptr, rows, pointer)
 
             elif special_key==b'P': #Down
-                if not line+offset==len(arr)+banoff-1:
-                    if not line==rows+banoff: line+=1
-                    elif not line+offset==len(arr)+1: offset+=1
-                    text=arr[line+offset-banoff]
-                    pointer,oldptr=fixlenline(text, pointer, oldptr)
+                pointer, oldptr, text, offset, line =\
+                down(line, offset, arr, text, banoff, oldptr, rows, pointer)
  
             elif special_key==b'M': #Right
                 if not pointer>max_len: pointer+=1; oldptr=pointer
@@ -79,45 +36,19 @@ while True:
                 if not pointer==1: pointer-=1; oldptr=pointer
                     
             elif special_key==b'S': #Supr
-                if not pointer==max_len+1:
-                    p1=list(text); p1.pop(pointer-1)
-                    text="".join(p1)
-                elif not line+offset==1: #move all to previous line
-                    seltext=arr[line+offset-banoff+1]
-                    arr[line+offset-banoff+1]=text+seltext
-                    arr.pop(line+offset-banoff+1)
-                    text=text+seltext
+                text, arr =\
+                supr(pointer, max_len, text, offset, banoff, arr)
 
             elif special_key==b'G': pointer=1
             elif special_key==b'O': pointer=len(text)+1
                 
         elif key==b'\x08': #Delete
-            if not pointer==1: #Delete char
-                p1=list(text); p1.pop(pointer-2)
-                text="".join(p1); pointer-=1
-            else: #move all to previous line
-                if not offset+line-1==1:
-                    seltext=arr[line+offset-banoff-1]
-                    arr[line+offset-banoff-1]=seltext+text
-                    arr.pop(line+offset-banoff)
-                    pointer=len(seltext)+1
-                    text=seltext+text
-                    if not offset==0:
-                        offset-=1
-                    else: line-=1
+            line, offset, text, arr, pointer =\
+            delete(pointer, text, offset, line, arr, banoff)
 
         elif key==b'\r': #Return (adds new lines or moves text
-            seltext=[text[:pointer-1]]
-            p1=arr[:line+offset-banoff]
-            p2=arr[line+offset-banoff:]
-            if not len(text)==0:
-                seltext=[text[:pointer-1]]
-                arr=p1+seltext+p2
-                text=text[pointer-1:]
-                pointer=0
-            else: arr=p1+[""]+p2
-            if not line>rows+1: line+=1
-            else: offset+=1           
+            line, offset, arr, pointer, text =\
+            newline(text, pointer, offset, banoff, line, arr, rows)           
 
         elif key==b'\x13': #Ctrl + S (SAVE)
             out=open(filename,"w",encoding="UTF-8")
@@ -136,76 +67,19 @@ while True:
             
         elif key==b'\x10': #Ctrl + P (PASTE TEXT)
             if not len(copy_buffer)==0:
-                p1=arr[:line+offset-banoff]
-                p2=arr[line+offset-banoff:]
-                arr=p1+[copy_buffer]+p2
-                text=copy_buffer
+                p1=arr[:line+offset-banoff]; p2=arr[line+offset-banoff:]
+                arr=p1+[copy_buffer]+p2; text=copy_buffer
     
         elif key==b'\x07': #Ctrl + G (go to line)
-            print("\r\033[%d;%dH"%(rows+banoff+2,1),end="")
-            print(" "+black+"Go to line:"+reset, end=" "); p1=input()
-            print("\r\033[%d;%dH"%(line, 1),end="")
-            try:
-                p1=int(p1)
-                if p1<len(arr):
-                    if p1<rows:
-                        offset=0
-                        line=p1+banoff
-                    else:
-                        offset=p1-rows
-                        line=rows+banoff
-            except: pass
+            line, offset = goto(rows, banoff, line, arr, offset, black, reset)
 
         elif key==b'\x01': #Ctrl + A (Save as)
-            saveastxt=black+" Save as:"+reset+" "
-            lenght=len(saveastxt)-16
-            filewrite=filename
-            wrtptr=lenght+len(filewrite)
-            while True:
-                print("\r\033[%d;%dH"%(rows+banoff+2, 1),end="")
-                print("\r"+" "*(len(filewrite)+lenght+1), end="")
-                print("\r "+saveastxt+filewrite,end="")
-                print("\r\033[%d;%dH"%(rows+banoff+2, wrtptr),end="")
-                key=getch() #Map keys
-                if key==b'\x13': #Ctrl + S (confirms)
-                    try:
-                        out=open(filewrite,"w",encoding="UTF-8")
-                        out.write("\n".join(arr)); out.close()
-                        status=saved_txt; status_st=2; break
-                    except: pass
-
-                elif key==b'\x11': #Ctrl + Q (cancel)
-                    break
-            
-                elif key==b'\x08': #Delete
-                    if not wrtptr==lenght:
-                        p1=list(filewrite); p1.pop(wrtptr-lenght-1)
-                        filewrite="".join(p1); wrtptr-=1
-
-                elif key==b'\xe0': #Arrows
-                    arrow=getch()
-                    if arrow==b'K': #Left
-                        if not wrtptr==lenght:
-                            wrtptr-=1
-                    elif arrow==b'M': #Right
-                        if not wrtptr>len(filewrite)+lenght-1:
-                            wrtptr+=1
-
-                else: #Rest of keys
-                    out=decode(key)
-                    p1=filewrite[:wrtptr-lenght]
-                    p2=filewrite[wrtptr-lenght:]
-                    filewrite=p1+out+p2
-                    wrtptr+=1
-                    
+            arr, status_st, filename =\
+            save_as(filename, black, reset, rows, banoff, arr, saved_txt)
+    
         else: #All the other keys
             p1=text[:pointer-1]; p2=text[pointer-1:]
-            #To read special key combinations like AltGr+4
-            for x in range(3):
-                try: out=key.decode("UTF-8"); break
-                except: key+=getch()
-            text=(p1+out+p2)
-            pointer+=1
-            status_st-=1
+            out=decode(key); text=(p1+out+p2); pointer+=1
+        status_st-=1
 
-    #except: pass
+    except: pass
