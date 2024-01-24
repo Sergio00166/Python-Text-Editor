7258ef1519eb441698e26761ba95a05106751fff
Sergio1260 <35770464+Sergio00166@users.noreply.github.com>
2024-01-24 18:17:22 +0100

    Add files via upload

--- bin/special_keys.py
+++ bin/special_keys.py
@@ -9,31 +9,31 @@ def special_keys(pointer,text,columns,offset,line,banoff,arr,rows,oldptr,max_len
 
     special_key=getch() #Read char
     
-    if special_key==b'H': #Up
+    if special_key==keys["arr_up"]:
         pointer, oldptr, text, offset, line =\
         up(line,offset,arr,text,banoff,oldptr,rows,pointer)
 
-    elif special_key==b'P': #Down
+    elif special_key==keys["arr_down"]:
         pointer, oldptr, text, offset, line =\
         down(line,offset,arr,text,banoff,oldptr,rows,pointer)
 
-    elif special_key==b'M': #Right
+    elif special_key==keys["arr_right"]:
         text, pointer, oldptr, line, offset =\
         right(pointer,text,columns,offset,line,banoff,arr,rows,oldptr)
             
-    elif special_key==b'K': #Left
+    elif special_key==keys["arr_left"]:
         pointer, oldptr, text, line, offset =\
         left(pointer,oldptr,line,offset,banoff,text,arr)
         
-    elif special_key==b'S': #Supr
+    elif special_key==keys["supr"]: #Supr
         text,arr = supr(pointer,max_len,text,offset,banoff,arr,line)
         status_st=False
 
-    elif special_key==b'G': #Start
+    elif special_key==keys["start"]: #Start
         pointer=1; p_offset=0
         oldptr=pointer
     
-    elif special_key==b'O': #End
+    elif special_key==keys["end"]: #End
         pointer=len(text)+1
         oldptr=pointer
 
