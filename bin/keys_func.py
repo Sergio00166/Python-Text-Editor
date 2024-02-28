c145b30d4633e15848654f2347afa5ecb87fd8cf
Sergio1260 <35770464+Sergio00166@users.noreply.github.com>
2024-02-28 09:23:33 +0100

    Minor changes
    Moved some key actions to another file

--- bin/keys_func.py
+++ bin/keys_func.py
@@ -2,6 +2,7 @@
 
 from functions import *
 from actions import *
+from actions1 import *
 from saveas import save_as
 from openfile import open_file
 
@@ -14,32 +15,12 @@ def keys_func(key,text,pointer,oldptr,line,offset,columns,banoff,arr,rows,\
         special_key=getch() #Read char
         
         if special_key==keys["arr_up"] or special_key==keys["ctrl+arr_up"]:
-            selected=special_key==keys["ctrl+arr_up"]
-            if selected: seled=[line,offset]
-            pointer, oldptr, text, offset, line =\
-            up(line,offset,arr,text,banoff,oldptr,rows,pointer)
-            if selected:
-                selst=[line,offset]
-                if not len(select)==0:
-                    select[0]=selst
-                else: select=[selst,seled]
-            else: select=[]
+            pointer, oldptr, text, offset, line, select =\
+            up(line,offset,arr,text,banoff,oldptr,rows,pointer,special_key,keys,select)
             
         elif special_key==keys["arr_down"] or special_key==keys["ctrl+arr_down"]:
-            
-            selected=special_key==keys["ctrl+arr_down"]
-            if selected:
-                selst=[line-banoff,offset]
-                fix=line+offset
-            pointer, oldptr, text, offset, line =\
-            down(line,offset,arr,text,banoff,oldptr,rows,pointer)
-            if selected:
-                seled=[line-banoff,offset]
-                if not len(select)==0:
-                    select[1]=seled
-                else: select=[selst,seled]
-            else: select=[]
-
+            pointer, oldptr, text, offset, line, select =\
+            down(line,offset,arr,text,banoff,oldptr,rows,pointer,special_key,keys,select)
 
         elif special_key==keys["arr_right"]:          
             text, pointer, oldptr, line, offset =\
@@ -50,14 +31,8 @@ def keys_func(key,text,pointer,oldptr,line,offset,columns,banoff,arr,rows,\
             left(pointer,oldptr,line,offset,banoff,text,arr)
             
         elif special_key==keys["supr"]:
-            if not len(select)==0:
-                p1=arr[:sum(select[0])]; p2=arr[sum(select[1]):]
-                line=select[0][0]+banoff; offset=select[0][1]
-                select=[];arr=p1+p2
-            else:
-                text,arr = supr(pointer,max_len,text,offset,banoff,arr,line)
-                if not sep==chr(92): getch()
-                status_st=False
+            text, arr, select, status_st =\
+            supr(pointer,max_len,text,offset,banoff,arr,line,select,status_st)
             
         elif special_key==keys["start"]:
             pointer=1; p_offset=0; oldptr=pointer
@@ -66,35 +41,22 @@ def keys_func(key,text,pointer,oldptr,line,offset,columns,banoff,arr,rows,\
             pointer=len(text)+1; oldptr=pointer
             
         elif special_key==keys["repag"]:
-            p1=line+offset-banoff-rows
-            if p1<0: p1=0
             line, offset, text =\
-            CalcRelLine(p1,arr,offset,line,banoff,rows)
-            if not sep==chr(92): getch()
+            repag(line,offset,banoff,rows,arr,sep)
             
         elif special_key==keys["avpag"]:
-            p1=line+offset-banoff+rows
-            if p1>=len(arr): p1="-"
             line, offset, text =\
-            CalcRelLine(p1,arr,offset,line,banoff,rows)
-            if not sep==chr(92): getch()
+            avpag(line,offset,banoff,rows,arr,sep)
 
-        
+
     elif key==keys["delete"]:
-        if not len(select)==0:
-            p1=arr[:sum(select[0])]; p2=arr[sum(select[1]):]
-            line=select[0][0]+banoff; offset=select[0][1]
-            select=[];arr=p1+p2
-        else:
-            line,offset, text, arr, pointer =\
-            backspace(pointer,text,offset,line,arr,banoff)
-            status_st=False
+        line,offset, text, arr, pointer, select =\
+        backspace(pointer,text,offset,line,arr,banoff,select)
 
     elif key==keys["return"]:
-        line,offset,arr,pointer,text =\
-        newline(text,pointer,offset,banoff,line,arr,rows)
-        status_st=False
-
+        line, offset, arr, pointer, text, staus =\
+        newline(text,pointer,offset,banoff,line,arr,rows,status)
+        
     elif key==keys["ctrl+s"]:
         out=open(filename,"w",encoding="UTF-8")
         out.write("\n".join(arr)); out.close()
@@ -102,47 +64,17 @@ def keys_func(key,text,pointer,oldptr,line,offset,columns,banoff,arr,rows,\
         out=open(filename,"r",encoding="UTF-8")
         
     elif key==keys["ctrl+x"]:
-        if not len(select)==0:
-            p1=arr[:sum(select[0])]; p2=arr[sum(select[1]):]
-            start=sum(select[0])-1
-            if start<0: start=0
-            copy_buffer=arr[start:sum(select[1])]
-            if not start==0: copy_buffer=copy_buffer[1:]
-            line=select[0][0]+banoff; offset=select[0][1]
-            select = []; arr = p1 + p2
-        elif line+offset>len(arr)-1:
-            copy_buffer=text[pointer-1:]
-            text=text[:pointer-1]
-        else:
-            copy_buffer=arr[line+offset-banoff]
-            arr.pop(line+offset-banoff)
-            text=arr[line+offset-banoff]
-        status_st=False
+        copy_buffer, arr, text, line, offset, select =\
+        cut(select,arr,line,offset,banoff,text,status_st,copy_buffer)
         
     elif key==keys["ctrl+c"]:
-        if not len(select)==0:
-            start=sum(select[0])-1
-            if start<0: start=0
-            copy_buffer=arr[start:sum(select[1])]
-            if not start==0: copy_buffer=copy_buffer[1:]
-        else: copy_buffer=arr[line+offset-banoff][pointer-1:]
-        select=[]
+        copy_buffer, select =\
+        copy(select,arr,line,offset,banoff,pointer)
         
     elif key==keys["ctrl+p"]:
-        if not len(copy_buffer)==0:
-            if isinstance(copy_buffer, list):
-                p1=arr[:line+offset-banoff]
-                p2=arr[line+offset-banoff:]
-                arr=p1+copy_buffer+p2
-                text = copy_buffer[0]
-                
-            else:
-                p1=arr[:line+offset-banoff]; p2=arr[line+offset-banoff+1:]
-                fix1=text[:pointer-1]; fix2=text[+pointer-1:]
-                out=fix1+copy_buffer+fix2; arr=p1+[out]+p2; text=out
-                pointer=len(fix1+copy_buffer); status_st=False
+        pointer, arr, text, status_st, copy_buffer =\
+        paste(copy_buffer,arr,line,offset,banoff,pointer,text,status_st)                                              
             
-
     elif key==keys["ctrl+g"]:
         line, offset ,text =\
         goto(rows,banoff,line,arr,offset,black,reset)
