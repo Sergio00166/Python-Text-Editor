15ec928466c47d4bbdcb1a8b0540962d62334498
Sergio Bañón <35770464+Sergio00166@users.noreply.github.com>
2024-09-25 09:21:26 +0200

    Update init.py

--- bin/init.py
+++ bin/init.py
@@ -26,7 +26,7 @@ if not __name__=="__main__":
     # Define colors
     init(autoreset=False,convert=True)
     reset=Style.RESET_ALL
-    bnc=Back.LIGHTWHITE_EX+Fore.BLACK+Style.DIM
+    bnc=Back.LIGHTWHITE_EX+Fore.BLACK
     black=Back.LIGHTCYAN_EX+Fore.BLACK+Style.DIM
     slc=Back.LIGHTYELLOW_EX+Fore.BLUE+Style.BRIGHT
     deinit(); del init, Fore, Back, Style, deinit
